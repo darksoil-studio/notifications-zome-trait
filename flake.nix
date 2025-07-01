80e855802d02cfce32d63015362dfe8a597a8969
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-01 17:29:06 +0200

    Trait

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Template for Holochain app development";
+
+  inputs = {
+    holonix.url = "github:holochain/holonix/main-0.5";
+
+    nixpkgs.follows = "holonix/nixpkgs";
+    flake-parts.follows = "holonix/flake-parts";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://holochain-ci.cachix.org"
+      "https://darksoil-studio.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8="
+      "darksoil-studio.cachix.org-1:UEi+aujy44s41XL/pscLw37KEVpTEIn8N/kn7jO8rkc="
+    ];
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = builtins.attrNames inputs.holonix.devShells;
+      perSystem = { inputs', config, pkgs, system, ... }: {
+        devShells.default =
+          pkgs.mkShell { inputsFrom = [ inputs'.holonix.devShells.default ]; };
+      };
+    };
+}
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use hc_zome_traits::*;
+use hdk::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Notification {
+    pub title: String,
+    pub body: String,
+    pub icon_src: String,
+    pub url_path_to_navigate_to_on_click: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetNotificationInput {
+    pub notification_id: String,
+    pub locale: String,
+}
+
+#[zome_trait]
+pub trait NotificationsZomeTrait {
+    fn get_notification(input: GetNotificationInput) -> ExternResult<Option<Notification>>;
+}
